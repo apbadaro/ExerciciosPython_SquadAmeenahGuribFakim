1fb8fb28d6e07c27c1ff7cdcf11fcb555a86fe87
aparecraft <149310690+aparecraft@users.noreply.github.com>
2024-01-17 14:23:58 -0300

    Update Conceito Básicos de Python - EXERCICIO 1.py

--- Conceito Básicos de Python - EXERCICIO 1.py
+++ Conceito Básicos de Python - EXERCICIO 1.py
@@ -14,4 +14,4 @@ print(f'A subtração dos números digitados foi {subtracao} ')
 print(f'A multiplicação dos números digitados foi {multiplicacao}')
 print(f'A divisão dos números digitados foi {divisao}')
 
-input("Pressione Enter para sair.")
+
