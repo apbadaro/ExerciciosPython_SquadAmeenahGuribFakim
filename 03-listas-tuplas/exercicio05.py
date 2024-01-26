0bb20f5e77c0f4729b5f1d16d5b421f5271bab81
Ana Paula Badaró <thisisanapaula@gmail.com>
2024-01-26 10:56:24 -0300

    adiciona exercício 5 de listas e tuplas

---
+++ 03-listas-tuplas/exercicio05.py
@@ -0,0 +1,7 @@
+# Crie duas tuplas. Concatene-as para formar uma nova tupla.
+
+people = ("Leia Organa", "Luke Skywalker", "Han Solo")
+droids = ("C-3PO", "R2-D2", "BB-8")
+star_wars_characters = people + droids
+
+print(star_wars_characters)
