75fd95cc737b9359ebf1e09525f47033a0fef8f5
Ana Paula <140989367+quasiEvil@users.noreply.github.com>
2024-02-07 12:59:44 -0300

    upload folder ExerciciosPOO

---
+++ ExerciciosPOO_SquadAmeenahGuribFakim/biblioteca.py
@@ -0,0 +1,71 @@
+from biblioteca_classes import Autor, Livro, Usuario
+
+
+def main():
+    # USUÁRIOS
+    usuario1 = Usuario("Leia Organa", "111222333", "Alderaan")
+    usuario2 = Usuario("Luke Skywalker", "444555666", "Tattooine")
+
+    # AUTORES
+    autor1 = Autor("JK Rowling", "0123456789", "Reino Unido", "jk@hogwarts.co.uk")
+    autor2 = Autor(
+        "Lewis Carroll", "9876543210", "Reino Unido", "carroll@rabbithole.co.uk"
+    )
+
+    # LIVROS
+    livro1 = Livro(
+        1,  # id
+        "Harry Potter e a Pedra Filosofal",  # título
+        "Mundo Mágico",  # editora
+        [autor1],  # lista porque pode haver um ou mais autores
+        ["Ficção, Fantasia, Infanto-juvenil"],  # exemplo de gêneros
+        3,  # exemplo de max renovação
+        2,  # exemplo de exemplares disponíveis
+        1,  # exemplo de exemplares emprestados
+    )
+    livro2 = Livro(
+        2,
+        "Harry Potter e a Câmara Secreta",
+        "Mundo Mágico",
+        [autor1],
+        ["Ficção, Fantasia, Infanto-juvenil"],
+        3,
+        2,
+        1,
+    )
+    livro3 = Livro(
+        3,
+        "Harry Potter e o Prisioneiro de Azkaban",
+        "Mundo Mágico",
+        [autor1],
+        ["Ficção, Fantasia, Infanto-juvenil"],
+        3,
+        2,
+        1,
+    )
+    livro4 = Livro(
+        4,
+        "Alice Através do Espelho",
+        "Lua Nova",
+        [autor2],
+        ["Ficção, Fantasia, Infanto-juvenil"],
+        4,
+        1,
+        3,
+    )
+
+    # TESTANDO SAÍDAS NO TERMINAL
+
+    print(
+        f"\n{usuario1.nome.upper()} alugou {livro2.titulo.upper()} e {livro3.titulo.upper()}.\n"
+    )
+    print(f"Usuário #2: {usuario2}")
+    print()
+    print(livro1)
+    print()
+    print(livro4)
+    print()
+
+
+if __name__ == "__main__":
+    main()
